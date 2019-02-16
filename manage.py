54c458fe17f983c30ee78fa1a5ebf0d3dcf249bf
zkhin <arandomname@gmail.com>
2019-02-16 00:36:33 -0500

    2

--- manage.py
+++ manage.py
@@ -1,20 +1,4 @@
 #!/usr/bin/env python
-import os
-import sys
-
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'epperson_project.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-=======
-#!/usr/bin/env python
 #test
 import os
 import sys
@@ -30,4 +14,3 @@ if __name__ == '__main__':
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
->>>>>>> bab525677b72147ad49942662cf855bc959b40e1
