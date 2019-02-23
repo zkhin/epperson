62898d0240f45770fcf9b6336450fa666028b1c7
zkhin <arandomname@gmail.com>
2019-02-23 03:12:20 -0500

    t

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'Zayar',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'February 19, 2019'
-    },
-    {
-        'author': 'Zayar',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'February 19, 2019'
-    }
-]
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
